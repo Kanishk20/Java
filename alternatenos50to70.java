5d31ae39f7702fd59dbca58575a80da91afa80fd
Kanishk Israni <67801143+Kanishk20@users.noreply.github.com>
2022-11-23 14:15:03 +0530

    Add files via upload

---
+++ alternatenos50to70.java
@@ -0,0 +1,17 @@
+// to display
+public class alternatenos50to70
+{
+    public static void main()
+    {
+        int i = 0;
+        for( i = 51; i <= 70; i = i+2)
+        {
+        
+            System.out.println(i);
+        }
+    }
+}
+            
+        
+            
+            
