a93eebe438d0ce98b0d32b6b6a55f360dfe66412
Kanishk Israni <67801143+Kanishk20@users.noreply.github.com>
2022-11-23 14:11:04 +0530

    Add files via upload

---
+++ A_to_Zusingloops.java
@@ -0,0 +1,20 @@
+// display
+class A_to_Zusingloops
+{
+    char c;
+    void compute_display()
+    {
+      for(c = 'A'; c <= 'Z'; c++)
+      System.out.println(c + " ");
+    }
+    
+ 
+    public static void main()
+    {
+      A_to_Zusingloops ob = new A_to_Zusingloops();
+      ob.compute_display();
+     
+    }
+}
+        
+
